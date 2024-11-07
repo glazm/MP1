16269a8a6c2f989bf85b10a02599780c173748c9
Michal Glaz <michal.glaz@op.pl>
2024-11-07 07:13:23 +0100

    Creating getters and setters for variables in Doctor and Contact class

--- src/mp1/model/Contact.java
+++ src/mp1/model/Contact.java
@@ -1,4 +1,69 @@
 package mp1.model;
 
 public class Contact {
+    private String streetName;
+    private int streetNumber;
+    private long telephoneNumber;
+    private String hospitalName;
+
+    public Contact(String streetName, int streetNumber, long telephoneNumber
+            ,String hospitalName){
+        setStreetName(streetName);
+        setStreetNumber(streetNumber);
+        setTelephoneNumber(telephoneNumber);
+        setHospitalName(hospitalName);
+
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        if(streetName == null || streetName.trim().isEmpty()){
+//            throw new ValException("streetName cannot be empty");
+        }
+        this.streetName = streetName;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        if(streetNumber<1){
+//            throw new ValException("streetNumber cannot be zero or negative");
+        }
+        this.streetNumber = streetNumber;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        if(hospitalName == null || hospitalName.trim().isEmpty()){
+//            throw new ValException("hospitalName cannot be empty");
+        }
+        this.hospitalName = hospitalName;
+    }
+
+    public long getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(long telephoneNumber) {
+        if(telephoneNumber<000000001&&telephoneNumber>999999999){
+//            throw new ValException("telephoneNumber must have exactly 9 digits");
+        }
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return  hospitalName + " " +
+                streetName + " " +
+                streetNumber + " " +
+                 telephoneNumber;
+    }
 }
--- src/mp1/model/Doctor.java
+++ src/mp1/model/Doctor.java
@@ -3,11 +3,61 @@ package mp1.model;
 import java.util.*;
 
 public class Doctor {
-    private long doctorId;
+    private long doctorId; //simple attribute
     private Set<String> patients = new HashSet<>();
     private Contact contact;
-    private String firstName;
-    private String surname;
+    private String firstName;//simple attribute
+    private String surname;//simple attribute
     public static int internshipInMonths = 13;
+    private Integer howManyCertificates;
 
+    private int yearOfBirth;//simple attribute
+    final static int retirementAge = 68;
+
+    public int tillRetirement(){
+        return retirementAge - yearOfBirth;
+    }
+
+    public long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(long doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        if(contact == null){
+//            throw new ValException("contact cannot be empty");
+        }
+        this.contact = contact;
+    }
 }
